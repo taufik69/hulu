0a07b981573bf0e5a6ed1e320654cb40c5baf97c
taufik69 <taufik35-2915@diu.edu.bd>
2023-04-08 22:31:58 +0600

    initail setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Menu from "./assets/component/Header/Menu";
+function App() {
+  return (
+    <>
+      <Menu />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary_color: "var(--primary_color)",
+      primary_Text_color: "var(--primary_Text_color)",
+      secendory_Text_color: "var(--secendory_Text_color)",
+      banner_color: "var(--banner_color)",
+      secondary_banner: "var(--secondary_banner)",
+      fotter_color: "var(--fotter_color)",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "2rem",
+        xl: "2rem",
+        "2xl": "1rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
